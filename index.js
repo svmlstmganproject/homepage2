b9597911eb5d8fbcbbb379f93f60d09282075500
kkabinsky <163501649+kkabinsky@users.noreply.github.com>
2024-08-22 18:05:37 +0700

    Initial commit homepage

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-let expess=require('express')
-let app=expess();
-port =process.env.port||3000;
+let express = require('express');  // Make sure this line is correct
+let app = express();
+const port = process.env.PORT || 3007;
 
-app.set('view engine','ejs');
-app.get('/home',(req,res)=>{
-    res.render('home')
-})
-app.get('/about',(req,res)=>{
-    res.render('about')
-})
-app.listen(port,()=>{
-    console.log('hellow')
-})
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));  // Correct use of express.static
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+});
